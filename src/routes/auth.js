3988a15e8b7279bbe40f0676c2db08494aab812d
Ross McLean <ross_mclean@posteo.net>
2021-03-15 17:49:59 +0000

    Remove old code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,38 +17,5 @@ router.get("/login", forwardAuthenticated, controller.get_login);
 router.post("/login", controller.authenticate_passport);
 
 router.get("/logout", controller.get_logout);
-// //VALIDATE BODY
-// const { email, password } = req.body;
-// if (!email || !password) {
-//   return res.status(401).render("auth/login", {
-//     message: "Please fill in all the fields"
-//   });
-// }
-// //MAKE SURE THE EMAIL IN THE BODY IS THE SAME AS ONE IN THE DATABASE
-// //COMPARE THE HASHED PASSWORD IN THE DATABASE TO THE PASSWORD IN THE BODY USING BCRYPT
-// const userWithEmail = await User.findOne({ email: email });
-// if (!userWithEmail) {
-//   return res.status(401).render("auth/login", {
-//     message: "Invalid Login Credentials"
-//   });
-// }
-// const validPassword = await bcrypt.compare(password, userWithEmail.password);
-// if (!validPassword) {
-//   return res.status(401).render("auth/login", {
-//     message: "Invalid Login Credentials"
-//   });
-// }
-
-// //CREATE A JSON WEB TOKEN FOR THE USER
-// const token = jwt.sign({ _id: userWithEmail._id }, process.env.JWT_SECRET);
-// // res.header("accessToken", token);
-// res.redirect(
-//   `/tournaments/myTournaments/?token=${encodeURIComponent(token)}`
-// );
-
-// // res.render("tournaments/myTournaments", {
-// //   token: token
-// // });
-// });
 
 module.exports = router;
