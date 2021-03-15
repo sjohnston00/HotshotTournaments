c48c6afc8c7e209a113118ec3cbc9f663484671b
Ross McLean <ross_mclean@posteo.net>
2021-03-15 17:51:48 +0000

    Remove unnecessary imports

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
 const controller = require("../controllers/authController");
 
 const { forwardAuthenticated, ensureAuthenticated } = require("../config/auth");
