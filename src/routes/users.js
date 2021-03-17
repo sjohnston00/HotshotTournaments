847aa69665c61c4c9318e110890455778e52467d
Ross McLean <ross_mclean@posteo.net>
2021-03-17 00:21:41 +0000

    Create userController.js - Move /deleteAllUsers get response to controller - Move /login post response to controller

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const controller = require("../controllers/usersController");
 
 //TODO: Delete in production
-router.get("/deleteAllUsers", async (req, res) => {
-  try {
-    const deletedUsers = await User.deleteMany({});
-    res.send(deletedUsers);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+router.get("/deleteAllUsers", controller.delete_all_users);
 
-router.post("/login", (req, res) => {});
+router.post("/login", controller.login);
 
 module.exports = router;
