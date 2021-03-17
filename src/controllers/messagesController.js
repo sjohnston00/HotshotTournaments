259c3c6c3f831aef4e897e91522e9df074ca2892
Ross McLean <ross_mclean@posteo.net>
2021-03-17 14:31:47 +0000

    What

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -1,1 +1,26 @@
 const Message = require("../models/Message");
+
+exports.post_message_to_tournament = async (req, res) => {
+    const { tournamentID } = req.params;
+    const message = new Message({
+        user: req.user._id,
+        isAnnouncement: req.body.isAnnouncement,
+        createdAt: new Date(),
+        tournament: tournamentID,
+        name: req.user.name,
+        body: req.body.message
+    });
+
+    try {
+        const savedMessage = await message.save();
+        await Tournament.updateOne(
+        { _id: tournamentID },
+        { $push: { messages: savedMessage._id } }
+        );
+        return res.json({ success: true, message: "message successfully added" });
+    } catch (error) {
+        console.log(error.message);
+        req.flash("error_msg", "Something went wrong, please try again later");
+        return res.redirect(`/tournaments/${tournamentID}`);
+    }
+}
