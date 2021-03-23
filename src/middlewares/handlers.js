298acaaa2a822f43a70490b2dfa8a73f0c11937c
Ross McLean <ross_mclean@posteo.net>
2021-03-23 17:42:38 +0000

    Remove (hopefully) unnecessary type conversion
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- src/middlewares/handlers.js
+++ src/middlewares/handlers.js
@@ -1,5 +1,5 @@
 exports.response_handler = (path, flashType, flashMsg, req, res, consoleErr = false) => {
   if (consoleErr) console.log(consoleErr) 
   req.flash(flashType, flashMsg)
-  res.redirect(String(path))
+  res.redirect(path)
 }
