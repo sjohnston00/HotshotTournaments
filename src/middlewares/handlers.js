7674c60f9cc7578ee909de0aaee757d1a10b0cdd
Ross McLean <ross_mclean@posteo.net>
2021-03-23 17:25:36 +0000

    Add support for console logs
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- src/middlewares/handlers.js
+++ src/middlewares/handlers.js
@@ -1,4 +1,5 @@
-exports.response_handler = (path, error, req, res) => {
+exports.response_handler = (path, flashType, flashMsg, req, res, consoleErr = false) => {
+  if (consoleErr) console.log(consoleErr) 
   req.flash('error_msg', error)
   res.redirect(String(path))
 }
