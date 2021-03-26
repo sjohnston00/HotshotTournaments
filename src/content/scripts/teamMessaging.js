d35b27a7438419888c22968c14e4087c0143e257
Sam Jonston <johnstonsam712@gmail.com>
2021-03-26 11:16:18 +0000

    Fixed broadcasting emmits to all chats
    Team chats are now private
    Moved Socket.io functionality to different file

--- src/content/scripts/teamMessaging.js
+++ src/content/scripts/teamMessaging.js
@@ -1,16 +1,16 @@
-const deployed = true;
+const deployed = false
 const connectionString = deployed
-  ? "https://hotshot-tournaments.herokuapp.com/"
-  : "http://localhost:8000";
-const socket = io.connect(connectionString);
+  ? 'https://hotshot-tournaments.herokuapp.com/'
+  : 'http://localhost:8000'
+const socket = io.connect(connectionString)
 
-const form = document.querySelector("#send-message-form");
-const message = document.querySelector("#input-message");
-const tournamentID = document.querySelector("#tournamentID").textContent;
-const teamID = document.querySelector("#teamID").textContent;
+const form = document.querySelector('#send-message-form')
+const message = document.querySelector('#input-message')
+const tournamentID = document.querySelector('#tournamentID').textContent
+const teamID = document.querySelector('#teamID').textContent
 
-socket.on("newMessage", (message) => {
-  const messagesContainer = document.querySelector("#messages-container");
+socket.on(`messageTeam:${teamID}`, (message) => {
+  const messagesContainer = document.querySelector('#messages-container')
 
   messagesContainer.innerHTML =
     messagesContainer.innerHTML +
@@ -21,25 +21,25 @@ socket.on("newMessage", (message) => {
       <p class="card-title">${message.body}</p>
     </div>
   </div>
-`;
-});
+`
+})
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
 
-  const messageValue = message.value;
+  const messageValue = message.value
   fetch(`/messages/team/${teamID}/tournament/${tournamentID}`, {
-    method: "POST",
+    method: 'POST',
     headers: {
       // "Content-Type": "application/json" //for when the application only accepts JSON input
-      "Content-Type": "application/x-www-form-urlencoded" //for when the application only accepts form urlencoded input
+      'Content-Type': 'application/x-www-form-urlencoded' //for when the application only accepts form urlencoded input
     },
     //when using FORM - URLENCODED use the URLSearchParams class fort the body
     body: new URLSearchParams({
       message: messageValue
     })
-  });
+  })
 
   //set the value of the message box back to empty
-  document.querySelector("#input-message").value = "";
-});
+  document.querySelector('#input-message').value = ''
+})
--- src/index.js
+++ src/index.js
@@ -90,32 +90,4 @@ const server = app.listen(PORT, () => {
   console.log(`App Started ===> Listening on port ${PORT}`)
 })
 
-const io = require('socket.io')(server)
-
-io.on('connection', (socket) => {
-  console.log('New user connected')
-})
-
-const connection = mongoose.connection
-connection.once('open', () => {
-  const messagesChangeStream = connection.collection('messages').watch()
-
-  messagesChangeStream.on('change', async (change) => {
-    switch (change.operationType) {
-      case 'insert':
-        const message = await Message.findById(
-          change.fullDocument._id
-        ).populate({ path: 'user', model: 'users', select: '-__v -password' })
-        console.log(message)
-        io.emit('newMessage', message)
-        break
-
-      case 'delete':
-        console.log(
-          `Deleted message: ${change.documentKey._id} from ${change.ns.db} collection ${change.ns.coll}`
-        )
-        io.emit('deletedMessage', change.documentKey._id)
-        break
-    }
-  })
-})
+const socket = require('./socket.js').createConnection(server)
